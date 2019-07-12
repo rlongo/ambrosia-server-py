21d73c95b78a2289b790cd4bb9c4b247cfcbe289
robert longo <robertflongo@gmail.com>
2019-07-11 21:02:53 -0400

    Added: upload file sanitization

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, config
 from flask_cors import CORS
 
 from api import ambrosia_api
 from asset_server import asset_server
 
 app = Flask(__name__)
+app.config['ASSETS_DIR'] = '/tmp/ambrosia'
+
 app.register_blueprint(ambrosia_api, url_prefix='/api/v1')
 app.register_blueprint(asset_server, url_prefix='/assets')
 CORS(app)
--- asset_server/routes.py
+++ asset_server/routes.py
@@ -1,22 +1,38 @@
 import os
-from flask import Blueprint, request, abort, session, send_from_directory
+from flask import Blueprint, request, abort, session, current_app, send_from_directory
+from werkzeug.utils import secure_filename
 
-UPLOAD_DIR='/tmp/ambrosia'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'bmp'])
 
-if not os.path.exists(UPLOAD_DIR):
-    os.makedirs(UPLOAD_DIR)
+def mkdir_assets(assets_dir):
+    if not os.path.exists(assets_dir):
+        os.makedirs(assets_dir)
 
 asset_server = Blueprint('asset_server', __name__)
 
+def allowed_filename(filename):
+    return '.' in filename and \
+            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 @asset_server.route('image/<path:path>', methods=['GET', 'POST'])
 def assets_image(path):
+    
+    path = secure_filename(path)
+        
+    if  path == '' or not allowed_filename(path):
+        abort(400, "Invalid filename")
 
+    assets_dir = current_app.config['ASSETS_DIR']
+    mkdir_assets(assets_dir)
+ 
     if request.method == 'POST':
         if 'file' not in request.files:
             abort(400, "no file provided")
+      
         f = request.files['file']
-        f.save(os.path.join(UPLOAD_DIR, path))
+       
+        f.save(os.path.join(assets_dir, path))
         return {'status': 'success'} , 201
 
     # GET
-    return send_from_directory(UPLOAD_DIR, path)
+    return send_from_directory(assets_dir, path)
